65a1047ef9801662e6b5fa0ea129c3b3ddb2d0c4
TimoBimo <hildebrandt.tim@live.de>
2023-05-01 21:51:56 +0200

    commit4

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { setupCounter } from "./counter.js";
 
 console.log(import.meta.env.VITE_MY_API_KEY);
 console.log(import.meta.env.VITE_ENVIRONMENT);
+console.log(import.meta.env.VITE_ENVIRONMENT);
 
 document.querySelector("#app").innerHTML = `
   <div>
