690929713d31dc9801c3c156285b695a6f3f24e6
TimoBimo <hildebrandt.tim@live.de>
2023-05-01 22:09:47 +0200

    commit05

--- main.js
+++ main.js
@@ -2,12 +2,39 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-
+import Airtable from "airtable";
 console.log(import.meta.env.VITE_MY_API_KEY);
-console.log(import.meta.env.VITE_ENVIRONMENT);
 console.log(import.meta.env.VITE_ENVIRONMENT);
 
 console.log("neuer test");
+const airtableApi = import.meta.env.VITE_AIRTABLE_API;
+var base = new Airtable({ apiKey: airtableApi }).base("appBb5j8BS7eDF0Y1");
+
+base("Table 1")
+	.select({
+		// Selecting the first 3 records in Grid view:
+		view: "Grid view",
+	})
+	.eachPage(
+		function page(records, fetchNextPage) {
+			// This function (`page`) will get called for each page of records.
+
+			records.forEach(function (record) {
+				console.log("Retrieved", record.get("Name"));
+			});
+
+			// To fetch the next page of records, call `fetchNextPage`.
+			// If there are more records, `page` will get called again.
+			// If there are no more records, `done` will get called.
+			fetchNextPage();
+		},
+		function done(err) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+		}
+	);
 
 document.querySelector("#app").innerHTML = `
   <div>
